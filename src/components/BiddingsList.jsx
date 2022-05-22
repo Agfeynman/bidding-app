84db90780ed32565dd78a90676582154d90ba85c
Andres Gutierrez <agfeynman@gmail.com>
2022-05-22 02:35:53 +0100

    feat: configure basic vite app

---
+++ src/components/BiddingsList.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { ActionCable } from 'react-actioncable-provider';
+import { API_ROOT } from '../constants';
+// import Cable from './Cable';
+// import NewConversationForm from './NewConversationForm';
+// import MessagesArea from './MessagesArea';
+
+const BiddingsList = () => {
+  const [biddings, setBiddings] = useState([]);
+  const [activeBidding, setActiveBidding] = useState('');
+
+  useEffect(() => {
+    return () => {
+      fetch(`${API_ROOT}/biddings`)
+        .then((res) => res.json())
+        .then((biddings) => {
+          console.log('bidiiidngs', biddings);
+          this.setBiddings(conversations);
+        });
+    };
+  }, []);
+
+  return <div>BiddingsList {biddings}</div>;
+};
+
+export default BiddingsList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ActionCableProvider url={API_WS_ROOT}>
+      <App />
+    </ActionCableProvider>
   </React.StrictMode>
-)
+);
